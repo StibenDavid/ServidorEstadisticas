0c3e75bc8377107c89e1c5421fe07f2194fbfaca
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-07-03 22:25:19 -0500

    mover paquetes

---
+++ GestionarEstadisticas/src/servidor/repositorios/OfertasRepositoryInt.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package servidor.repositorios;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface OfertasRepositoryInt {
+    public LinkedList<Float> obtenerOfertas();   
+}
